936fc89cc6ceb4f7579e9c490f777d856f7fa86f
coco <coco@lacueva.tv>
2020-08-14 01:18:33 +0000

    update ajax por recaptcha
    tuve que cambiar el sistema de rectaptcha que no funcionaba

--- web/inc/ajax.php
+++ web/inc/ajax.php
@@ -38,11 +38,21 @@ if( isAjax() ) {
 			$siteKey = '6Lf0BkwUAAAAAFiIZs1E1Ux8ugLd_T9TURIQRQon';
 			$secret = '6Lf0BkwUAAAAAB-rjShQ9CgITdlSzHR16FShFJEl';
 			
-			//$recaptcha = new \ReCaptcha\ReCaptcha($secret);
-			$recaptcha = new \ReCaptcha\ReCaptcha($secret, new \ReCaptcha\RequestMethod\SocketPost());
-			$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);			
+			// call curl to POST request
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => $secret, 'response' => $_POST['g-recaptcha-response'])));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$response = curl_exec($ch);
+			curl_close($ch);
+			$arrResponse = json_decode($response, true);
 			
-			if ( $resp->isSuccess() ) {
+			//$recaptcha = new \ReCaptcha\ReCaptcha($secret);
+			//$recaptcha = new \ReCaptcha\ReCaptcha($secret, new \ReCaptcha\RequestMethod\SocketPost());
+			//$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);			
+			//var_dump($resp);
+			if ( $arrResponse["success"] == '1' ) {
 
 				require_once('lib/PHPMailer/src/PHPMailer.php');
 				require_once('lib/PHPMailer/src/SMTP.php');
